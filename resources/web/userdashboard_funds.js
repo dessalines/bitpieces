3ae116b69572b5f99c0e0233636a9030e53f0e17
tyler <tyler@tyler-Latitude-E6420>
2014-07-13 11:58:02 -0400

    added tablesorting, still don't have UTC dates converting correctly yet

--- resources/web/userdashboard_funds.js
+++ resources/web/userdashboard_funds.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 
 	sessionId = getCookie("authenticated_session_id");
 	console.log(sessionId);
-
+    
 	fillUserHighChartStandardTemplate('get_users_funds_accum', '#users_funds');
 	fillTableFromMustache('get_users_transactions', '#transactions_template', '#transactions');
 });
@@ -21,13 +21,16 @@ $(document).ready(function(){
       success: function(data, status, xhr) {
       	console.log(data);
             // var jsonObj = JSON.parse(data);
-            var jsonObj = jQuery.parseJSON(data);
+            // JSON.useDateParser();
+            // var jsonObj = jQuery.parseJSON(data);
+            JSON.useDateParser();
+            var jsonObj = JSON.parse(data);
             
             var template = $(templateId).html();
             Mustache.parse(template);   // optional, speeds up future uses
             var rendered = Mustache.render(template,jsonObj);
             $(divId).html(rendered);
-
+			$("#transactions_table").tablesorter({debug: true}); 
             console.log(jsonObj);              
             console.log(template);
             console.log(rendered);
