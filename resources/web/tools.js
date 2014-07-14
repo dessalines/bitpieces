8cc63566c656e986b977dd36f9d1847fab555ba6
tyler <tyler@tyler-Latitude-E6420>
2014-07-14 13:29:22 -0400

    Nice titles and breadcrumbs for your user stuff

--- resources/web/tools.js
+++ resources/web/tools.js
@@ -103,4 +103,38 @@ function fillUserHighChartPieChartTemplate(url, id) {
         }
     });
 
+
 }
+
+function fillUserInfoMustache(url) {
+       var url = "http://localhost:4567/" + sessionId + "/" + url// the script where you handle the form input.
+       $.ajax({
+        type: "GET",
+        url: url,
+        xhrFields: {
+          withCredentials: true
+        },
+      // data: seriesData, 
+      success: function(data, status, xhr) {
+        console.log(data);
+            // var jsonObj = JSON.parse(data);
+            var jsonObj = jQuery.parseJSON(data);
+            
+            var template = $('#usernameTemplate').html();
+            Mustache.parse(template);   // optional, speeds up future uses
+            var rendered = Mustache.render(template,jsonObj);
+            $('#userDropdown').html(rendered);
+
+            console.log(jsonObj);              
+            console.log(template);
+            console.log(rendered);
+            
+
+          },
+          error: function (request, status, error) {
+
+            toastr.error(request.responseText);
+          }
+        });
+
+     }
