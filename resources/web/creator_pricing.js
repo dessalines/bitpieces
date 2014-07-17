73b8e41da7b73a01fc70060ce7d8db9431517e4a
tyler <tyler@tyler-Latitude-E6420>
2014-07-17 11:32:50 -0400

    Got a non-responsive right sidebar working for the creators overview

--- resources/web/creator_pricing.js
+++ resources/web/creator_pricing.js
@@ -3,4 +3,6 @@ $(document).ready(function(){
 
 	var creatorName = getParameterByName('creator');
 	fillUserHighChartStandardTemplate(creatorName + '/get_pricing', '#pricing', 'Price ($/piece)', '$');
+	fillTableFromMustache(creatorName + '/get_bids_asks', '#bids_asks_template', '#bids_asks', '#bids_asks_table');
+	fillTableFromMustache(creatorName + '/get_pieces_issued', '#pieces_issued_template', '#pieces_issued', '#pieces_issued_table');
 });
