0066ea0003a93e8bfa1f4cc3bff84678ad006699
tyler <tyler@localhost>
2014-08-04 10:31:32 -0400

    added scheduled deployer

--- deploy.sh
+++
@@ -1,3 +0,0 @@
-rm bitpieces_practice-0.0.1-SNAPSHOT-jar-with-dependencies.jar*
-wget https://github.com/tchoulihan/bitpieces/releases/download/release_test2/bitpieces_practice-0.0.1-SNAPSHOT-jar-with-dependencies.jar
-java -cp bitpieces_practice-0.0.1-SNAPSHOT-jar-with-dependencies.jar com.bitpieces.dev.web_service.WebService
