1bea1cca840c5cf5d39f0f8cc944da8d2bf89a6e
tyler <tyler@localhost>
2014-08-04 10:54:05 -0400

    again

--- resources/stage/web/tools.js
+++ resources/stage/web/tools.js
@@ -767,16 +767,6 @@ function showRecaptcha(element) {
 
 function setupDisqus(creatorName) {
 
-    /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
-    var disqus_shortname = 'bitpieces_' + creatorName; // required: replace example with your forum shortname
 
-    /* * * DON'T EDIT BELOW THIS LINE * * */
-    (function() {
-        var dsq = document.createElement('script');
-        dsq.type = 'text/javascript';
-        dsq.async = true;
-        dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-    })();
 
 }
