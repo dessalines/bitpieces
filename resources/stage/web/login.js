b8374200fcc346b6a9dbbc518f902354a8b95857
tyler <tyler@localhost>
2014-08-06 10:43:10 -0400

    cleaning up, proper login validation

--- resources/stage/web/login.js
+++ resources/stage/web/login.js
@@ -18,138 +18,40 @@ $(document).ready(function() {
     setupCreatorSearch();
     fillUserInfoMustacheFromCookie();
     setupLogout();
+    showHideElementsLoggedIn();
+    showRecaptcha("recaptcha_div");
 
     console.log(document.cookie);
+
     a = $('#registerForm').bootstrapValidator({
         message: 'This value is not valid',
         excluded: [':disabled'],
         submitButtons: 'button[type="submit"]'
 
     }).on('success.form.bv', function(event) {
-        event.preventDefault;
-        alert('hai2');
-        setupRegisterBtn();
+        event.preventDefault();
+        setupRegisterBtnsetupRegisterAjax();
+
 
     });
 
 
     $('#loginForm').bootstrapValidator({
         message: 'This value is not valid',
-        excluded: [':disabled']
+        excluded: [':disabled'],
+        submitButtons: 'button[type="submit"]'
 
-    });
-
-    showHideElementsLoggedIn();
-    showRecaptcha("recaptcha_div");
-
-
-
-    // $('#registerForm').submit(function(){
-    //  $.each($('#registerForm').serializeArray(), function(i, field) {
-    //         values[field.name] = field.value;
-    //         console.log(values);
-    //  });
-
-
-    // });
-
-
-
-
-
-    $("#signinBtn").click(function(event) {
-
-        // serializes the form's elements.
-        var formData = $("#loginForm").serializeArray();
-        console.log(formData);
-
-        // Loading
-        $(this).button('loading');
-
-
-        var url;
-        // Decide if its a creator or not
-        var isCreator = $("#creatorCheckbox").is(':checked')
-        console.log(isCreator);
-        if (isCreator) {
-            url = sparkService + "creatorlogin";
-        } else {
-            url = sparkService + "userlogin";
-        }
-
-        // username = $('#userLoginDiv').find('input[name="username"]').val();
-        // // = $("#inputUsername3").val();
-        // document.cookie = "username=" + username ;
-
-
-        $.ajax({
-            type: "POST",
-            url: url,
-            xhrFields: {
-                withCredentials: true
-            },
-            data: formData,
-            success: function(data, status, xhr) {
-
-                //    console.log(xhr);
-                // console.log(asdf);
-                // console.log(xhr.getAllResponseHeaders()); 
-
-                // alert(data); // show response from the php script.
-
-                xhr.getResponseHeader('Set-Cookie');
-
-                // old way
-                // document.cookie="authenticated_session_id=" + data + 
-                // "; expires=" + expireTimeString(60*60); // 1 hour (field is in seconds)
-                // Hide the modal, reset the form, show successful
-                $("#userloginModal").modal('hide');
-                $('#loginForm')[0].reset();
-
-                toastr.success('Logged in.');
-
-                showHideElementsLoggedIn();
-
-
-
-                console.log(document.cookie);
-                console.log(formData.username);
-
-                // GO to the dashboard
-                if (!isCreator) {
-                    window.location.replace("userdashboard_overview?user=" + formData[0]['value']);
-                } else {
-                    console.log(formData);
-                    var url = "creator_overview?creator=" + formData[0]['value'];
-                    console.log(url);
-                    window.location.replace(url);
-
-                }
-
-
-            },
-            error: function(request, status, error) {
-                delete_cookie("authenticated_session_id");
-                toastr.error(request.responseText);
-            }
-
-
-
-        });
-
-        $("#signinBtn").button('reset');
+    }).on('success.form.bv', function(event) {
         event.preventDefault();
-        return false;
+        setupSigninAjax();
     });
 
 
-
-    // Logging out
 
 
 });
 
-function setupRegisterBtn() {
+function setupRegisterAjax() {
     // !!!!!!They must have names unfortunately
     // $("#registerBtn").click(function(event) {
 
@@ -194,11 +96,95 @@ function setupRegisterBtn() {
         error: function(request, status, error) {
             delete_cookie("authenticated_session_id");
             toastr.error(request.responseText);
+
         }
     });
 
-
-
-    event.preventDefault();
     // });
+}
+
+function setupSigninAjax() {
+
+    // serializes the form's elements.
+    var formData = $("#loginForm").serializeArray();
+    console.log(formData);
+
+    // Loading
+    $(this).button('loading');
+
+
+    var url;
+    // Decide if its a creator or not
+    var isCreator = $("#creatorCheckbox").is(':checked')
+    console.log(isCreator);
+    if (isCreator) {
+        url = sparkService + "creatorlogin";
+    } else {
+        url = sparkService + "userlogin";
+    }
+
+    // username = $('#userLoginDiv').find('input[name="username"]').val();
+    // // = $("#inputUsername3").val();
+    // document.cookie = "username=" + username ;
+
+
+    $.ajax({
+        type: "POST",
+        url: url,
+        xhrFields: {
+            withCredentials: true
+        },
+        data: formData,
+        success: function(data, status, xhr) {
+
+            //    console.log(xhr);
+            // console.log(asdf);
+            // console.log(xhr.getAllResponseHeaders()); 
+
+            // alert(data); // show response from the php script.
+
+            xhr.getResponseHeader('Set-Cookie');
+
+            // old way
+            // document.cookie="authenticated_session_id=" + data + 
+            // "; expires=" + expireTimeString(60*60); // 1 hour (field is in seconds)
+            // Hide the modal, reset the form, show successful
+            $("#userloginModal").modal('hide');
+            $('#loginForm')[0].reset();
+
+            toastr.success('Logged in.');
+
+            showHideElementsLoggedIn();
+
+
+
+            console.log(document.cookie);
+            console.log(formData.username);
+
+            // GO to the dashboard
+            if (!isCreator) {
+                window.location.replace("userdashboard_overview?user=" + formData[0]['value']);
+            } else {
+                console.log(formData);
+                var url = "creator_overview?creator=" + formData[0]['value'];
+                console.log(url);
+                window.location.replace(url);
+
+            }
+
+
+        },
+        error: function(request, status, error) {
+            delete_cookie("authenticated_session_id");
+            toastr.error(request.responseText);
+        }
+
+
+
+    });
+
+    $("#signinBtn").button('reset');
+    event.preventDefault();
+    return false;
+
 }
