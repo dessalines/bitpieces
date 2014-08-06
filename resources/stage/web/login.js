981682a4db62a5da88cb2a6f219f9c88518efbee
tyler <tyler@localhost>
2014-08-06 10:45:36 -0400

    a fix

--- resources/stage/web/login.js
+++ resources/stage/web/login.js
@@ -30,7 +30,7 @@ $(document).ready(function() {
 
     }).on('success.form.bv', function(event) {
         event.preventDefault();
-        setupRegisterBtnsetupRegisterAjax();
+        setupRegisterAjax();
 
 
     });
