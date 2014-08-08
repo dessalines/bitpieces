a8dca1207373c537d915a1ffa9dd1821190d48ec
tyler <happydooby@gmail.com>
2014-08-08 11:23:20 -0400

    removed testweb

--- resources/web/testweb/creator_worth.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-
-    var creatorName = window.location.pathname.split('/').pop();
-    fillUserHighChartStandardTemplate(creatorName + '/get_worth', '#worth', 'Worth ($)', '$');
-
-});
--- resources/web/testweb/userdashboard_prices.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-
-    var userName = window.location.pathname.split('/').pop();
-
-
-
-    fillUserHighChartStandardTemplate(userName + '/get_prices_for_user', '#highcharts-chart-area', 'Price ($/piece)', '$');
-
-});
