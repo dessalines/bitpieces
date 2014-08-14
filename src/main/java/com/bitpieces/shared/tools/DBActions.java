8692fab9fedd9f57c981f0648180c321699e2098
tyler <happydooby@gmail.com>
2014-08-14 14:11:34 -0400

    a

--- src/main/java/com/bitpieces/shared/tools/DBActions.java
+++ src/main/java/com/bitpieces/shared/tools/DBActions.java
@@ -453,8 +453,9 @@ public class DBActions {
 		try {
 			System.out.println("got here1");
 			Currencies usd = Currencies.findFirst("iso=?", "USD");
-			
-			String encryptedPass = new StrongPasswordEncryptor().encryptPassword(postMap.get("password"));
+			StrongPasswordEncryptor encryptor = new StrongPasswordEncryptor();
+			System.out.println("got here1.1");
+			String encryptedPass = encryptor.encryptPassword(postMap.get("password"));
 			System.out.println("got here1.2");
 			User user = User.createIt(
 					"username", postMap.get("username"),
