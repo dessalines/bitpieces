f40cb35addccd25bdf2d604f00590044e96f29ec
tyler <happydooby@gmail.com>
2014-08-14 09:53:29 -0400

    require not allowed here

--- resources/virtual_hosts_script.sh
+++ resources/virtual_hosts_script.sh
@@ -30,7 +30,6 @@ MAIN_CONF="<VirtualHost *:80>
     DocumentRoot /var/www/bitpieces.com/public_html
     ErrorLog ${APACHE_LOG_DIR}/error.log
     CustomLog ${APACHE_LOG_DIR}/access.log combined
-    Require all granted
 </VirtualHost>
 "
 
@@ -43,7 +42,6 @@ TEST_CONF="<VirtualHost *:80>
     DocumentRoot /var/www/test.bitpieces.com/public_html
     ErrorLog ${APACHE_LOG_DIR}/error.log
     CustomLog ${APACHE_LOG_DIR}/access.log combined
-    Require all granted
 </VirtualHost>
 "
 
