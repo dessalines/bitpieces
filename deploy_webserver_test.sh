b5f8e0a66aa67fc74e012096f432880b2277d935
tyler <tyler@bitpieces.com>
2014-08-16 09:16:55 -0400

    adding kill

--- deploy_webserver_test.sh
+++ deploy_webserver_test.sh
@@ -1,3 +1,4 @@
 git pull
 mvn install
+ps aux | grep -ie com.bitpieces.dev.web_service.WebService | awk '{print $2}' | xargs kill -9 
 java -cp target/bitpieces_practice-0.0.1-SNAPSHOT-jar-with-dependencies.jar com.bitpieces.dev.web_service.WebService
