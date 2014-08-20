9975e2b387cc923af3c622a928e0bed8caa0c2a6
tyler <happydooby@gmail.com>
2014-08-20 09:28:46 -0400

    a

--- resources/testweb/creatorpage.js
+++ resources/testweb/creatorpage.js
@@ -433,60 +433,8 @@ function setupModal(shortUrl, formId, buttonId, modalId) {
 
     }).on('success.form.bv', function(event) {
         event.preventDefault();
-        // serializes the form's elements.
-        var formData = $(formId).serializeArray();
-
-        // console.log(formData);
-
-        // Loading
-        $(this).button('loading');
-
-        // username = $('#userLoginDiv').find('input[name="username"]').val();
-        // // = $("#inputUsername3").val();
-        // document.cookie = "username=" + username ;
-
-        var url = sparkService + shortUrl;
-        $.ajax({
-            type: "POST",
-            url: url,
-            xhrFields: {
-                withCredentials: true
-            },
-            data: formData,
-            success: function(data, status, xhr) {
-
-                //    console.log(xhr);
-                // console.log(asdf);
-                // console.log(xhr.getAllResponseHeaders()); 
-
-                // alert(data); // show response from the php script.
-
-                xhr.getResponseHeader('Set-Cookie');
-
-                // old way
-                // document.cookie="authenticated_session_id=" + data + 
-                // "; expires=" + expireTimeString(60*60); // 1 hour (field is in seconds)
-                // Hide the modal, reset the form, show successful
-                $(modalId).modal('hide');
-                $(formId)[0].reset();
-
-                toastr.success(data);
-
-
-
-            },
-            error: function(request, status, error) {
-                toastr.error(request.responseText);
-            }
-
-
-
-        });
-
-        $(buttonId).button('reset');
-        event.preventDefault();
-        return false;
-
+        standardFormPost(shortUrl, formId, modalId, true);
+        
     });
 
 
