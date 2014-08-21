9f665eada58adc7874d52e03a806aff57de90013
tyler <happydooby@gmail.com>
2014-08-21 16:23:57 -0400

    a

--- resources/virtual_hosts_script.sh
+++ resources/virtual_hosts_script.sh
@@ -23,7 +23,7 @@ sudo rm /etc/apache2/sites-available/test.bitpieces.com.conf
 
 
 
-MAIN_CONF="<VirtualHost *:443>
+MAIN_CONF="<VirtualHost *:80>
     ServerAdmin admin@bitpieces.com
     ServerName bitpieces.com
     ServerAlias www.bitpieces.com
