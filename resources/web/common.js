836c58ee366d7c21df56e3dde6c6b12f9c4f3252
tyler <happydooby@gmail.com>
2014-08-21 14:43:50 -0400

    corrected stage port

---
+++ resources/web/common.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+    var userType = getCookie('usertype');
+    var userName = getCookie('username');
+    // set up the correct dashboard if its a creator
+    if (userType == 'Creator') {
+        $("#dashboardhref").prop("href", "/creators/main/" + userName);
+    } else if (userType == 'User') {
+        $("#dashboardhref").prop("href", "/users/overview/" + userName);
+    }
+
+
+    setupCreatorSearch();
+    fillUserInfoMustacheFromCookie();
+    setupLogout();
+    showHideElementsLoggedIn();
+
+
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-54002327-1', 'auto');
+  ga('send', 'pageview');
+
+    
+});
