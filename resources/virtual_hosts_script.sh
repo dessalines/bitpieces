6667f4e524475e45230c7286dc03fe3058c402c2
tyler <happydooby@gmail.com>
2014-08-22 09:04:41 -0400

    a

--- resources/virtual_hosts_script.sh
+++ resources/virtual_hosts_script.sh
@@ -24,26 +24,47 @@ sudo rm /etc/apache2/sites-available/test.bitpieces.com.conf
 
 # Now, its all about changing the default-ssl to have those 3 lines below
 
-# MAIN_CONF="<VirtualHost *:80>
-#     ServerAdmin admin@bitpieces.com
-#     ServerName bitpieces.com
-#     ServerAlias www.bitpieces.com
-#     DocumentRoot /var/www/html/bitpieces.com/public_html
-#     ErrorLog ${APACHE_LOG_DIR}/error.log
-#     CustomLog ${APACHE_LOG_DIR}/access.log combined
-#     SSLEngine on
-#     SSLProtocol all
-#     SSLCertificateFile /home/tyler/public.crt
-#     SSLCertificateKeyFile /home/tyler/privatekey.key
-#     SSLCACertificateFile /home/tyler/intermediate.crt
-#      # <Directory /var/www/html/bitpieces.com/public_html>
-#      # Options Indexes FollowSymLinks
-#      #   Require all granted
-#     #Directory>
-# </VirtualHost>
-# "
+MAIN_CONF="
+          <VirtualHost _default_:443>
+                ServerAdmin admin@bitpieces.com
+                ServerName bitpieces.com
+                ServerAlias www.bitpieces.com
+                DocumentRoot /var/www/html/bitpieces.com/public_html
 
-# --echo "$MAIN_CONF" | sudo tee -a /etc/apache2/sites-available/bitpieces.com.conf
+                ErrorLog ${APACHE_LOG_DIR}/error.log
+                CustomLog ${APACHE_LOG_DIR}/access.log combined
+
+                SSLEngine on
+
+                    SSLProtocol all
+                    SSLCertificateFile /home/tyler/public.crt
+                    SSLCertificateKeyFile /home/tyler/privatekey.key
+                    SSLCACertificateFile /home/tyler/intermediate.crt
+                <FilesMatch "\.(cgi|shtml|phtml|php)$">
+                                SSLOptions +StdEnvVars
+                </FilesMatch>
+                <Directory /usr/lib/cgi-bin>
+                                SSLOptions +StdEnvVars
+                </Directory>
+
+                BrowserMatch 'MSIE [2-6]' \
+                                nokeepalive ssl-unclean-shutdown \
+                                downgrade-1.0 force-response-1.0
+                # MSIE 7 and newer should be able to use keepalive
+                BrowserMatch 'MSIE [17-9]' ssl-unclean-shutdown
+
+        </VirtualHost>
+
+<VirtualHost *:80>
+    ServerAdmin admin@bitpieces.com
+    ServerName bitpieces.com
+    ServerAlias www.bitpieces.com
+    Redirect / https://bitpieces.com/
+</VirtualHost>
+
+"
+
+echo "$MAIN_CONF" | sudo tee -a /etc/apache2/sites-available/bitpieces.com.conf
 
 TEST_CONF="<VirtualHost *:80>
     ServerAdmin admin@test.bitpieces.com
@@ -63,9 +84,10 @@ echo "$TEST_CONF" | sudo tee -a /etc/apache2/sites-available/test.bitpieces.com.
 cd /etc/apache2/sites-available/
 sudo a2enmod rewrite
 sudo a2enmod ssl
-# sudo a2ensite bitpieces.com.conf
-sudo a2ensite test.bitpieces.com.conf
 sudo a2ensite default-ssl.conf
+sudo a2ensite bitpieces.com.conf
+sudo a2ensite test.bitpieces.com.conf
+
 sudo service apache2 restart
 
 cd ~/git/bitpieces/resources
