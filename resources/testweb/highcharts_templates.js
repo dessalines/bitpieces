1941f409a77313c8d8375cde5bd192cbc9115dfc
tyler <happydooby@gmail.com>
2014-08-22 23:15:27 -0400

    a

--- resources/testweb/highcharts_templates.js
+++ resources/testweb/highcharts_templates.js
@@ -59,6 +59,12 @@ function standardTemplate(id, seriesData, yAxisLabel, symbol) {
 }
 
 function pieChartTemplate(id, seriesData) {
+    Highcharts.setOptions({
+        colors: ['#50B432', '#ED561B', '#DDDF00', '#24CBE5', '#64E572', '#FF9655', '#FFF263', '#6AF9C4'],
+        global: {
+            useUTC: false
+        }
+    });
     $(id).highcharts({
         chart: {
             plotBackgroundColor: null,
