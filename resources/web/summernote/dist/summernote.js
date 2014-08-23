2edded1045e8ec2bf50c508298ef9e59cfe613cf
tyler <happydooby@gmail.com>
2014-08-23 17:18:04 -0400

    a

--- resources/web/summernote/dist/summernote.js
+++ resources/web/summernote/dist/summernote.js
@@ -3319,7 +3319,7 @@
       var dropdown = options.dropdown;
 
       return '<button type="button"' +
-                 ' class="btn btn-default btn-sm btn-small' +
+                 ' class="btn btn-primary btn-sm btn-small' +
                    (className ? ' ' + className : '') +
                    (dropdown ? ' dropdown-toggle' : '') +
                  '"' +
