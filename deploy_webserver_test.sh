49f158f3cd367c39b8994833f89e490ebf9f19dc
tyler <happydooby@gmail.com>
2014-08-25 13:09:17 -0400

    mmmk

--- deploy_webserver_test.sh
+++ deploy_webserver_test.sh
@@ -3,4 +3,4 @@ mvn install
 ps aux | grep -ie com.bitpieces.dev.web_service.WebService | awk '{print $2}' | xargs kill -9 
 nohup java -cp target/bitpieces_practice-0.0.1-SNAPSHOT-jar-with-dependencies.jar com.bitpieces.dev.web_service.WebService &> /dev/null &
 sleep 10s
-wget -r -nd --delete-after --no-check-certificate http://test.bitpieces.com:4566/pass_encrypt
+wget -r -nd --delete-after --no-check-certificate http://test.bitpieces.com:4566/pass_encrypt &> /dev/null &
