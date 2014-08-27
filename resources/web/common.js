5c3c05f36884ffcea8abcfacbdcbc5556d7b792f
tyler <happydooby@gmail.com>
2014-08-27 11:08:46 -0400

    different tracking code for web from testweb

--- resources/web/common.js
+++ resources/web/common.js
@@ -20,7 +20,7 @@ $(document).ready(function() {
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-54002327-1', 'auto');
+  ga('create', 'UA-54159960-1', 'auto');
   ga('send', 'pageview');
 
     
